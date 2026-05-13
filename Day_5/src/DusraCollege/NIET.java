a20f0eb34922783f04d4a21f845e58ddae9d3ec3
Abhinav Choudhary <Choudharyabhinav51@gmail.com>
2026-05-13 16:14:26 +0530

    Day_5_Learning

---
+++ Day_5/src/DusraCollege/NIET.java
@@ -0,0 +1,9 @@
+package DusraCollege;
+
+//import defaultDemo.CollegeOffice;
+public class NIET {
+    public static void main(String[] args) {
+    //    CollegeOffice o = new CollegeOffice(); //cannot create object because collegeOffice class is default not public
+    //    System.out.println(o.internalNotice); // cannot access because internalNotice is not public in defaultDemo.CollegeOffice; cannot be accessed from outside package
+    }
+}
---
+++ Day_5/src/protectedDemo/College.java
@@ -0,0 +1,9 @@
+package protectedDemo; // 1st package
+
+public class College {
+    protected String Announcement = "15 Dec ki daru party hai..";
+
+    protected void rules(){
+        System.out.println("Daru nhi pini hai...");
+    }
+}
