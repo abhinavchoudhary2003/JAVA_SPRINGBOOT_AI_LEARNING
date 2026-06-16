2284c281bea6f35d3516c4cc690c90dc6f430b9d
Abhinav Choudhary <Choudharyabhinav51@gmail.com>
2026-06-16 11:41:06 +0530

    OOPS_Practice_Questions

---
+++ OOPs/src/PracticeQuestions.java
@@ -0,0 +1,33 @@
+public class PracticeQuestions {
+
+    // Q1
+//        public static void main(String[] args) {
+//            Car c = new Car(); // creating an object with constructor call automatically
+//        }
+//}
+//      class Car {
+//    Car() { // constructor
+//        System.out.println("Car created");
+//    }
+//}
+// output is Car created because Constructor is called automatically when new Car() is executed.
+
+    // Question 2
+
+  /*  public static void main(String[] args) {
+        Student s = new Student(95);
+        System.out.println(s.marks);
+    }
+}
+class Student {
+    int marks;
+    Student(int marks) { // constructor with parameter
+        this.marks = marks;
+    }
+}
+// output is 95
+   */
+
+    // Question 3
+}
+
